2df5053bb618a0bf4be56eb347c1d85021f7da92
HsiaoCz <984274788@qq.com>
2024-12-26 06:35:57 +0800

    add some

--- santino/types/messages.go
+++ santino/types/messages.go
@@ -1,4 +1,17 @@
 package types
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Messages struct {
+	MessageID  uuid.UUID `json:"message_id"`
+	SenderID   uuid.UUID `json:"sender_id"`
+	ReceiverID uuid.UUID `json:"receiver_id"`
+	Content    string    `json:"content"`
+	CreatedAt  time.Time `json:"created_at"`
+	IsRead     bool      `json:"is_read"`
+	IsDeleted  bool      `json:"is_deleted"`
 }
--- santino/types/user_devices.go
+++ santino/types/user_devices.go
@@ -1,4 +1,15 @@
 package types
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type UserDevices struct {
+	DeviceID    int64     `json:"device_id"`
+	UserID      uuid.UUID `json:"user_id"`
+	DeviceName  string    `json:"device_name"`
+	DeviceToken string    `json:"device_token"`
+	LastLogin   time.Time `json:"last_login"`
 }
