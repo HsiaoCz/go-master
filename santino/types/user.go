9a9ae6905c4d11718d4e0d41abe82508b072aae1
HsiaoCz <984274788@qq.com>
2025-02-12 11:13:57 +0800

    add some

--- santino/types/user.go
+++ santino/types/user.go
@@ -6,6 +6,31 @@ import (
 	"github.com/google/uuid"
 )
 
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e *Error) Error() string {
+	return e.Message
+}
+
+var (
+	ErrUsernameEmpty = &Error{
+		Code:    400,
+		Message: "Username cannot be empty",
+	}
+	ErrEmailEmpty = &Error{
+		Code:    400,
+		Message: "Email cannot be empty",
+	}
+
+	ErrPasswordEmpty = &Error{
+		Code:    400,
+		Message: "Password cannot be empty",
+	}
+)
+
 type Users struct {
 	UserID         uuid.UUID `gorm:"primaryKey" json:"user_id"`
 	Username       string    `json:"username"`
@@ -19,4 +44,40 @@ type Users struct {
 	LastSeen       time.Time `json:"last_seen"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
+}
+
+type UserRegister struct {
+	Username   string `json:"username"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	FullName   string `json:"full_name"`
+	Phone      string `json:"phone_number"`
+	Avatar     string `json:"avatar"`
+	Background string `json:"background_url"`
+}
+
+func (u *UserRegister) Validate() error {
+	if u.Username == "" {
+		return ErrUsernameEmpty
+	}
+	if u.Email == "" {
+		return ErrEmailEmpty
+	}
+	if u.Password == "" {
+		return ErrPasswordEmpty
+	}
+	return nil
+}
+
+func (u *Users) HashPassword() error {
+	hash, err := HashPassword(u.PasswordHash)
+	if err != nil {
+		return err
+	}
+	u.PasswordHash = hash
+	return nil
+}
+
+func HashPassword(password string) (string, error) {
+	return password, nil
 }
